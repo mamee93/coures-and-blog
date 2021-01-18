37bb0bb51c956b097ed663c1aa3b19806c383412
maram <mameeal@gmail.com>
2021-01-18 21:00:54 +0400

    Add App Posts

---
+++ posts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+app_name="posts"
+urlpatterns = [
+    #path('', views.index,name="index"),
+    #path('blog', views.blog,name="post-list"),
+    #path('search', views.search,name="search"),
+    #path('create', views.post_create ,name="post-create"),
+    #path('post/<int:id>/', views.post,name="post-detail"),
+    #path('post/<int:id>/update', views.post_update ,name="post-update"),
+    #path('post/<int:id>/delete', views.post_delete ,name="post-delete"),
+
+]
